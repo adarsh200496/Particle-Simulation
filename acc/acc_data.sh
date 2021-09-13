967ce6e86b2601ab1297dd8c27a1038e5f45bb19
Toby Isaac <tisaac@cc.gatech.edu>
2021-09-13 11:23:44 -0400

    Remove stray OUTPUT_FILE

--- acc/acc_data.sh
+++ acc/acc_data.sh
@@ -7,7 +7,7 @@ make clean
 
 make main
 
-echo "[" > $OUTPUT_FILE
+echo "["
 
 FIRST_ITER="yes"
 for inputs in "1000 5" "1000 10" "100 15" "10 20" "10 25" "10 30" "10 35" "10 40" "10 45" "10 50" "10 55" "10 60"; do
