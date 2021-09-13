f4cfa6e60b9035ca3b1e86e1611b09ef858393ad
Toby Isaac <tisaac@cc.gatech.edu>
2021-09-13 11:29:42 -0400

    Add openmp script

--- openmp/omp_data.sh
+++ openmp/omp_data.sh
@@ -1,6 +1,6 @@
 # We want as reproducible an environment as possible
 module purge
-module load git nvhpc
+module load git intel
 
 # clean temporary files
 make clean
@@ -10,8 +10,7 @@ make main
 echo "["
 
 FIRST_ITER="yes"
-#for inputs in "100000 5" "1000 10" "100 15" "10 20" "10 25" "10 30" "10 35" "10 40" "10 45" "10 50" "10 55" "10 60"; do
-for inputs in "100000 5" "1000 10"; do
+for inputs in "100000 5" "1000 10" "100 15" "10 20" "10 25" "10 30" "10 35" "10 40" "10 45" "10 50" "10 55" "10 60"; do
   if [ "$FIRST_ITER" = "yes" ]; then
     FIRST_ITER="no"
   else
